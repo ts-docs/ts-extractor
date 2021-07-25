de2f124cd619718579db1eedaaeca0cfe85067b7
GoogleFeud <volen.sl666@gmail.com>
2021-07-25 13:26:29 +0300

    Clearly defined structure

--- src/structure.ts
+++ src/structure.ts
@@ -2,13 +2,19 @@
 export interface Module {
     name: string,
     modules: Array<Module>,
-    classes: Array<unknown>,
-    functions: Array<unknown>,
-    interfaces: Array<unknown>,
-    types: Array<unknown>,
-    enums: Array<unknown>,
-    constants: Array<unknown>,
+    classes: Array<ClassDecl>,
+    functions: Array<FunctionDecl>,
+    interfaces: Array<InterfaceDecl>,
+    types: Array<TypeDecl>,
+    enums: Array<EnumDecl>,
+    constants: Array<ConstantDecl>,
     sourceFile: string
+}
+
+export interface Node {
+    name: string,
+    start: number,
+    end: number
 }
 
 export function createModule(name: string, sourceFile: string) : Module {
@@ -23,4 +29,104 @@ export function createModule(name: string, sourceFile: string) : Module {
         enums: [],
         constants: []
     };
+}
+
+export interface Reference {
+    name: string,
+    path?: Array<string>,
+    typeParameters?: Array<TypeParameter>
+}
+
+export interface Literal {
+    stringified: string,
+    object?: ObjectLiteral | ArrowFunction | Union; 
+}
+
+export type TypeOrLiteral = Reference | Literal;
+
+export interface TypeParameter extends Node {
+    default?: TypeOrLiteral,
+    constraint?: Reference
+}
+
+export interface ClassMember extends Node {
+    isPublic?: boolean,
+    isPrivate?: boolean,
+    isStatic?: boolean,
+    isProtected?: boolean
+}
+
+export interface ClassProperty extends ClassMember {
+    type?: TypeOrLiteral,
+    typeParameters?: TypeParameter,
+    optional?: boolean,
+    exclamation?: boolean
+}
+
+export interface FunctionParameter extends Node {
+    type?: TypeOrLiteral,
+    isRest?: boolean,
+    isOptional?: boolean,
+    defaultValue?: string
+}
+
+export interface ClassMethod extends ClassMember {
+    typeParameters?: Array<TypeParameter>,
+    returnType?: TypeOrLiteral,
+    parameters?: Array<FunctionParameter>
+}
+
+export interface ClassDecl extends Node {
+    typeParameters: Array<TypeParameter>,
+    properties: Array<ClassProperty>,
+    methods: Array<ClassMethod>,
+    extends?: Reference,
+    implements?: Reference
+}
+
+export interface FunctionDecl extends Node {
+    typeParameters?: Array<TypeParameter>,
+    returnType?: TypeOrLiteral,
+    parameters?: Array<FunctionParameter>
+}
+
+export interface ArrowFunction extends Omit<Node, "name"> {
+    typeParameters?: Array<TypeParameter>,
+    returnType?: TypeOrLiteral,
+    parameters?: Array<TypeOrLiteral>
+}
+
+export interface ObjectLiteral extends Omit<Node, "name">  {
+    properties: Array<InterfaceProperty>
+}
+
+export interface Union extends Omit<Node, "name"> {
+    left: TypeOrLiteral,
+    right: TypeOrLiteral
+}
+
+export interface InterfaceProperty extends Node {
+    type: TypeOrLiteral,
+    isOptional: boolean
+}
+
+export interface InterfaceDecl extends Node {
+    properties: Array<InterfaceProperty>,
+}
+
+export interface TypeDecl extends Node {
+    value: TypeOrLiteral
+}
+
+export interface ConstantDecl extends Node {
+    type?: TypeOrLiteral,
+    content: string
+}
+
+export interface EnumMember extends Node {
+    initializer?: string
+}
+
+export interface EnumDecl extends Node {
+    members: Array<EnumMember>
 }
