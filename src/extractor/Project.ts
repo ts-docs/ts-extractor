d4ce9c00447d61b37d5cebd0e5dd941c4f25f1ee
GoogleFeud <volen.sl666@gmail.com>
2021-09-17 11:45:09 +0300

    Bug fixes, more declaration handlers

--- src/extractor/Project.ts
+++ src/extractor/Project.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import path from "path";
 import ts from "typescript";
@@ -43,50 +44,41 @@ export class Project {
         this.moduleCache = {};
     }
 
-    visitor(sourceFile: ts.SourceFile | ts.Symbol, currentModule = this.module) : void {
-        const sym = "kind" in sourceFile ? this.checker.getSymbolAtLocation(sourceFile) : sourceFile;
+    visitor(sourceFile: ts.SourceFile|ts.Symbol) : void {
+        let sym;
+        if ("fileName" in sourceFile) sym = this.checker.getSymbolAtLocation(sourceFile);
+        else sym = sourceFile;
         if (!sym || !sym.exports) return;
-        //@ts-expect-error You should be able to iterate through symbol.exports
+        for (const val of this.checker.getExportsOfModule(sym)) this.handleSymbol(val);
+
+        // @ts-expect-error You should be able to do that
         for (const [, val] of sym.exports) {
             // export * from "..."
             if (val.name === "__export") {
                 for (const decl of val.declarations!) {
                     if (ts.isExportDeclaration(decl) && decl.moduleSpecifier && ts.isStringLiteral(decl.moduleSpecifier)) {
-                        const reExportedFile = this.program.getSourceFile(path.join(process.cwd(), decl.getSourceFile().fileName, "../", `${decl.moduleSpecifier.text}.ts`));
+                        const reExportedFile = this.resolveSourceFile(decl.getSourceFile().fileName, decl.moduleSpecifier.text);
                         if (!reExportedFile) return;
-                        this.visitor(reExportedFile, this.getOrCreateModule(reExportedFile));
+                        this.visitor(reExportedFile);
                     }
                 } 
             } else {
-                let type;
-                if (!val.declarations || !val.declarations.length) return;
-                if (hasBit(val.flags, ts.SymbolFlags.Class)) type = "class";
-                else if (hasBit(val.flags, ts.SymbolFlags.Interface)) type = "interface";
-                else if (hasBit(val.flags, ts.SymbolFlags.Enum)) type = "enum";
-                else if (hasBit(val.flags, ts.SymbolFlags.TypeAlias)) type = "type alias";
-                else if (hasBit(val.flags, ts.SymbolFlags.Namespace)) type = "namespace";
-                else if (hasBit(val.flags, ts.SymbolFlags.Variable)) type = "variable";
-                else if (hasBit(val.flags, ts.SymbolFlags.Function)) type = "function";
-                // export { ... } from "...";
-                // export { ... };
-                else {
-                    const aliased = this.resolveAliasedSymbol(val);       
-                    const mod = createModule(val.name, false, undefined, true);
-                    currentModule.modules.set(val.name, mod);
-                    this.visitor(aliased, mod);
-                    continue;
-                }
-                console.log(type, val.name, currentModule.name);
+                this.handleSymbol(val);
+                //console.log(type, val.name, currentModule.name);
             }
         }
+
     }
 
-    getOrCreateModule(source: ts.SourceFile) : Module {
-        const {dir} = path.parse(source.fileName);
+    getOrCreateModule(source: string) : Module {
+        const {dir} = path.parse(source);
         if (this.moduleCache[dir]) return this.moduleCache[dir];
         let paths = dir.split("/");
         paths = paths.slice(paths.indexOf(this.baseDir) + 1);
-        if (!paths.length) return this.module;
+        if (!paths.length) {
+            this.moduleCache[dir] = this.module;
+            return this.module;
+        }
         let lastModule = this.module;
         for (const pathPart of paths) {
             const newMod = lastModule.modules.get(pathPart);
@@ -97,6 +89,7 @@ export class Project {
             } 
             else lastModule = newMod;
         }
+        this.moduleCache[dir] = lastModule;
         return lastModule;
     }
 
@@ -110,13 +103,66 @@ export class Project {
         return undefined;
     }
 
-    handleClassDecl(_classDecl: ts.ClassDeclaration, currentModule: Module) : void {
-        console.log("IN MODULE: ", currentModule.name);
+    handleSymbol(val: ts.Symbol) : void {
+        if (!val.declarations || !val.declarations.length) return;
+        if (hasBit(val.flags, ts.SymbolFlags.Class)) this.handleClassDecl(val.declarations[0] as ts.ClassDeclaration);
+        else if (hasBit(val.flags, ts.SymbolFlags.Interface)) this.handleInterfaceDecl(val.declarations as Array<ts.InterfaceDeclaration>);
+        else if (hasBit(val.flags, ts.SymbolFlags.Enum)) this.handleEnumDecl(val.declarations as Array<ts.EnumDeclaration>);
+        else if (hasBit(val.flags, ts.SymbolFlags.TypeAlias)) this.handleTypeAliasDecl(val.declarations[0] as ts.TypeAliasDeclaration);
+        else if (hasBit(val.flags, ts.SymbolFlags.Module)) this.handleNamespaceDecl(val.declarations as Array<ts.ModuleDeclaration>);
+        else if (hasBit(val.flags, ts.SymbolFlags.Variable)) this.handleVariableDecl(val.declarations[0] as ts.VariableDeclaration);
+        else if (hasBit(val.flags, ts.SymbolFlags.Function)) this.handleFunctionDecl(val.declarations[0] as ts.FunctionDeclaration);
+        else {
+            const aliased = this.resolveAliasedSymbol(val);
+            //if (aliased.name === "unknown") console.log(val.name, val.flags, val.declarations.length);
+            if (aliased.name.includes("/")) return this.visitor(aliased);
+            else this.handleSymbol(aliased);
+        }
+    }
+
+    handleClassDecl(_classDecl: ts.ClassDeclaration, _currentModule = this.getOrCreateModule(_classDecl.getSourceFile().fileName)) : void {
+        //console.log("Class: ", _classDecl.name?.text, " In module: ", _currentModule.name);
+    }
+
+    handleInterfaceDecl(_decls: Array<ts.InterfaceDeclaration>, _currentModule = this.getOrCreateModule(_decls[0].getSourceFile().fileName)) : void {
+        //console.log("Interface in module: ", currentModule.name);
+    }
+
+    handleEnumDecl(_decls: Array<ts.EnumDeclaration>, _currentModule = this.getOrCreateModule(_decls[0].getSourceFile().fileName)) : void {
+        //console.log("Enum in module: ", currentModule.name);
+    }
+
+    handleTypeAliasDecl(_decl: ts.TypeAliasDeclaration, _currentModule = this.getOrCreateModule(_decl.getSourceFile().fileName)) : void {
+        //console.log("Type alias in module: ", currentModule.name);
+    }
+
+    handleNamespaceDecl(_decls: Array<ts.ModuleDeclaration>, _currentModule = this.getOrCreateModule(_decls[0].getSourceFile().fileName)) : void {
+        //console.log("Type alias in module: ", currentModule.name);
+    }
+
+    handleVariableDecl(_decl: ts.VariableDeclaration, _currentModule = this.getOrCreateModule(_decl.getSourceFile().fileName)) : void {
+        //console.log("Type alias in module: ", currentModule.name);
+    }
+
+    handleFunctionDecl(_decl: ts.FunctionDeclaration, _currentModule = this.getOrCreateModule(_decl.getSourceFile().fileName)) : void {
+        //console.log("Type alias in module: ", currentModule.name);
     }
 
     resolveAliasedSymbol(symbol: ts.Symbol) : ts.Symbol {
         while (hasBit(symbol.flags, ts.SymbolFlags.Alias)) symbol = this.checker.getAliasedSymbol(symbol);
         return symbol;
+    }
+
+    resolveSourceFile(filePath: string, relative: string) : ts.SourceFile|undefined {
+        let res;
+        if (path.isAbsolute(filePath)) {
+            res = this.program.getSourceFile(path.join(filePath, "../", `${relative}.ts`));
+            if (!res) res = this.program.getSourceFile(path.join(filePath, "../", `${relative}/index.ts`));
+        } else {
+            res = this.program.getSourceFile(path.join(process.cwd(), filePath, "../", `${relative}.ts`));
+            if (!res) res = this.program.getSourceFile(path.join(process.cwd(), filePath, "../", `${relative}/index.ts`));
+        }
+        return res;
     }
 
 }
